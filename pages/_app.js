a4f884abc7654cdafe5d6c88169c51ec62a86e83
Vicroni <drasa_tec@hotmail.com>
2019-08-06 23:15:00 -0500

    Restablecer algunas cosas

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 
-import { ApolloProvider } from 'react-apollo';
-import { createHttpLink } from 'apollo-link-http';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-const link = createHttpLink({
-  uri: "http://localhost:4000/",
-  credentials: 'include'
-});
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-  dataIdFromObject: o => o.id
-});
-
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
@@ -33,11 +17,9 @@ class MyApp extends App {
 
     return (
       <Container>
-        <ApolloProvider client={client} >
           Este es un header
           <Component {...pageProps} />
           Este es un footer
-        </ApolloProvider>
       </Container>
     );
   }
