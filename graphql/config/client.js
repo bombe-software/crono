e8c48a1604f5f3dcb3b6db9dc30549d2a11cbfc6
vicroni <drasa_tec@hotmail.com>
2019-09-19 16:54:23 -0500

    Correcion de cronos

--- graphql/config/client.js
+++ graphql/config/client.js
@@ -1,10 +1,12 @@
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http';
+import fetch from 'node-fetch';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const link = createHttpLink({
-    uri: "http://localhost:4000/"
+    uri: "http://localhost:4000/",
+    fetch
 });
 
 export default new ApolloClient({
