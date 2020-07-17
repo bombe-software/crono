5efd2b054f493cae45e6ebe4a475e367afd2aeba
MedinaVilla <net_medina@hotmail.com>
2020-07-17 14:24:17 -0500

    Layout carpet to footer and header

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 
-<<<<<<< HEAD
-import Footer from './../components/layout/Footer';
-import Header from './../components/layout/Header';
-=======
 import Header from './../components/layout/header';
 import Footer from './../components/layout/footer';
->>>>>>> 0d601f37bc26d84025bdd9f23e131c148a426278
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -26,17 +21,11 @@ class MyApp extends App {
 
     return (
       <Container>
-<<<<<<< HEAD
-            <Header />
-            <Component {...pageProps} />
-            <Footer/>
-=======
         <Header />
         <div class="ui main text container">
           <Component {...pageProps} />
         </div>
         <Footer />
->>>>>>> 0d601f37bc26d84025bdd9f23e131c148a426278
       </Container>
     );
   }
