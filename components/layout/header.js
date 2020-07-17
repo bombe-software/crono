88d25b2c7e825a3ae6dea686d41891516dbf43af
MedinaVilla <net_medina@hotmail.com>
2020-07-17 14:34:05 -0500

    classname correction

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,29 +1,29 @@
 export default (props) => {
     return (
         <div>
-            <div class="ui fixed menu">
-                <div class="ui container">
-                    <a href="#" class="header item">
-                        <img class="logo" src="" />
+            <div className="ui fixed menu">
+                <div className="ui container">
+                    <a href="#" className="header item">
+                        <img className="logo" src="" />
                     Crono
                 </a>
-                    <a href="#" class="item">Home</a>
-                    <div class="ui simple dropdown item">
-                        Dropdown <i class="dropdown icon"></i>
-                        <div class="menu">
-                            <a class="item" href="#">Link Item</a>
-                            <a class="item" href="#">Link Item</a>
-                            <div class="divider"></div>
-                            <div class="header">Header Item</div>
-                            <div class="item">
-                                <i class="dropdown icon"></i>
+                    <a href="#" className="item">Home</a>
+                    <div className="ui simple dropdown item">
+                        Dropdown <i className="dropdown icon"></i>
+                        <div className="menu">
+                            <a className="item" href="#">Link Item</a>
+                            <a className="item" href="#">Link Item</a>
+                            <div className="divider"></div>
+                            <div className="header">Header Item</div>
+                            <div className="item">
+                                <i className="dropdown icon"></i>
                             Sub Menu
-                            <div class="menu">
-                                    <a class="item" href="#">Link Item</a>
-                                    <a class="item" href="#">Link Item</a>
+                            <div className="menu">
+                                    <a className="item" href="#">Link Item</a>
+                                    <a className="item" href="#">Link Item</a>
                                 </div>
                             </div>
-                            <a class="item" href="#">Link Item</a>
+                            <a className="item" href="#">Link Item</a>
                         </div>
                     </div>
                 </div>
