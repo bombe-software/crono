a0fca636ed8c3fe5719c1e30fa6f70e984c3a5cf
sgonzalezd <sgonzalezd@ciencias.unam.mx>
2020-07-22 14:16:03 -0500

    Cambios rapidos

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -4,22 +4,20 @@ const Header = () => {
     return (
         <div>
             <div className="ui segment top fixed menu">
-                <div className="item">
-                    <h4 className="ui header">
-                        <img className="ui image" src="/images/logoCronos.svg" />
+                <div>
+                    <h3 className="ui header">
+                        <img className="ui image" src="/images/logoCronos.svg" width="100" />
                         <div className="content">
                             Cronos
                         </div>
-                    </h4>
+                    </h3>
                 </div>
                 <div className="ui secondary right menu">
-                    <a className="item active">Home</a>
-                    <a className="item">Messages</a>
-                    <a className="item"> Friends</a>
+                    <a className="item active">Horario</a>
+                    <a className="item">Agenda</a>
+                    <a className="item"> Amigos</a>
                 </div>
             </div>
-            <br></br>
-            <br></br>
             <br></br>
             <br></br>
             <br></br>
