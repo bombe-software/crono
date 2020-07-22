058996825b3023c830eb9f49151a0453905e5f9c
MedinaVilla <net_medina@hotmail.com>
2020-07-22 14:10:56 -0500

    sa

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,44 +1,45 @@
-export default (props) => {
+import React from 'react';
+
+const Footer = () => {
     return (
-    <div className="ui vertical footer segment">
-        <div className="ui section divider"></div>
+        <div  className="ui inverted vertical footer segment">
         <div className="ui center aligned container">
-            <div className="ui stackable divided grid">
+            <div className="ui stackable inverted divided grid">
                 <div className="three wide column">
-                    <h4 className="ui header">Group 1</h4>
-                <div className="ui link list">
-                <a href="#" className="item">Link One</a>
-                <a href="#" className="item">Link Two</a>
-                <a href="#" className="item">Link Three</a>
-                <a href="#" className="item">Link Four</a>
+                    <h4 className="ui inverted header">Group 1</h4>
+                    <div className="ui inverted link list">
+                        <a href="#" className="item">Link One</a>
+                        <a href="#" className="item">Link Two</a>
+                        <a href="#" className="item">Link Three</a>
+                        <a href="#" className="item">Link Four</a>
+                    </div>
+                </div>
+                <div className="three wide column">
+                    <h4 className="ui inverted header">Group 2</h4>
+                    <div className="ui inverted link list">
+                        <a href="#" className="item">Link One</a>
+                        <a href="#" className="item">Link Two</a>
+                        <a href="#" className="item">Link Three</a>
+                        <a href="#" className="item">Link Four</a>
+                    </div>
+                </div>
+                <div className="three wide column">
+                    <h4 className="ui inverted header">Group 3</h4>
+                    <div className="ui inverted link list">
+                        <a href="#" className="item">Link One</a>
+                        <a href="#" className="item">Link Two</a>
+                        <a href="#" className="item">Link Three</a>
+                        <a href="#" className="item">Link Four</a>
+                    </div>
+                </div>
+                <div className="seven wide column">
+                    <h4 className="ui inverted header">Bombe Software</h4>
+                    <p>Make it simple, make it great</p>
+                </div>
             </div>
-        </div>
-        <div className="three wide column">
-          <h4 className="ui header">Group 2</h4>
-          <div className="ui link list">
-            <a href="#" className="item">Link One</a>
-            <a href="#" className="item">Link Two</a>
-            <a href="#" className="item">Link Three</a>
-            <a href="#" className="item">Link Four</a>
-          </div>
-        </div>
-        <div className="three wide column">
-          <h4 className="ui header">Group 3</h4>
-          <div className="ui link list">
-            <a href="#" className="item">Link One</a>
-            <a href="#" className="item">Link Two</a>
-            <a href="#" className="item">Link Three</a>
-            <a href="#" className="item">Link Four</a>
-          </div>
-        </div>
-        <div className="seven wide column">
-          <h4 className="ui header">Un producto de: </h4>
-          <img src="/static/img/bombe-text-logo.png" className="ui centered small image" />
-          <p>Make it simple, make it great</p>
-        </div>
-      </div>
-      <div className="ui section divider"></div>
-        <div className="ui horizontal small divided link list">
+            <div className="ui inverted section divider"></div>
+            <img src="assets/images/logo.png" className="ui centered mini image" />
+            <div className="ui horizontal inverted small divided link list">
                 <a className="item" href="#">Site Map</a>
                 <a className="item" href="#">Contact Us</a>
                 <a className="item" href="#">Terms and Conditions</a>
@@ -48,3 +49,5 @@ export default (props) => {
     </div>
     );
 }
+
+export default Footer;
--- components/layout/header.js
+++ components/layout/header.js
@@ -1,37 +1,35 @@
-export default (props) => {
+import React from 'react';
+
+const Header = () => {
     return (
-        <div>
-            <div className="ui fixed menu">
-                <div className="ui container">
-                    <a href="#" className="header item">
-                        <img className="logo" src="" />
-                    Crono
+        <div style={{position: "relative"}} className="ui fixed inverted menu">
+            <div className="ui container">
+                <a href="#" className="header item">
+                    <img className="logo" src="assets/images/logo.png" />
+                    Project Name
                 </a>
-                    <a href="#" className="item">Home</a>
-                    <div className="ui simple dropdown item">
-                        Dropdown <i className="dropdown icon"></i>
-                        <div className="menu">
-                            <a className="item" href="#">Link Item</a>
-                            <a className="item" href="#">Link Item</a>
-                            <div className="divider"></div>
-                            <div className="header">Header Item</div>
-                            <div className="item">
-                                <i className="dropdown icon"></i>
+                <a href="#" className="item">Home</a>
+                <div className="ui simple dropdown item">
+                    Dropdown <i className="dropdown icon"></i>
+                    <div className="menu">
+                        <a className="item" href="#">Link Item</a>
+                        <a className="item" href="#">Link Item</a>
+                        <div className="divider"></div>
+                        <div className="header">Header Item</div>
+                        <div className="item">
+                            <i className="dropdown icon"></i>
                             Sub Menu
                             <div className="menu">
-                                    <a className="item" href="#">Link Item</a>
-                                    <a className="item" href="#">Link Item</a>
-                                </div>
+                                <a className="item" href="#">Link Item</a>
+                                <a className="item" href="#">Link Item</a>
                             </div>
-                            <a className="item" href="#">Link Item</a>
                         </div>
                         <a className="item" href="#">Link Item</a>
                     </div>
                 </div>
             </div>
-            <br></br>
-            <br />
-            <br />
         </div>
     );
 }
+
+export default Header;
